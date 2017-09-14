62a97cccf16dd2569dbd7507b005a43dc45bc71b
vivianlotus <31488849+vivianlotus@users.noreply.github.com>
2017-09-14 14:03:47 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,13 @@
 
+## No.1
+
+#download and unzip data
 if(!file.exists("./data")) dir.create("./data")
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "./data/projectData_getCleanData.zip")
 listZip <- unzip("./data/projectData_getCleanData.zip", exdir = "./data")
 
-
+#load data into R
 train.x <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 train.y <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 train.subject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
@@ -12,29 +15,35 @@ test.x <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
 test.y <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
 test.subject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
+#merge data
 trainData <- cbind(train.subject, train.y, train.x)
 testData <- cbind(test.subject, test.y, test.x)
 fullData <- rbind(trainData, testData)
 
+## No.2
 
-
+#load feauture name into R
 featureName <- read.table("./data/UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)[,2]
+
+#extract mean and std
 featureIndex <- grep(("mean\\(\\)|std\\(\\)"), featureName)
 finalData <- fullData[, c(1, 2, featureIndex+2)]
 colnames(finalData) <- c("subject", "activity", featureName[featureIndex])
 
+## No.3
 
+#load activity data into R
 activityName <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+
+#label the data set with descriptive variable names
 finalData$activity <- factor(finalData$activity, levels = activityName[,1], labels = activityName[,2])
-
-
 names(finalData) <- gsub("\\()", "", names(finalData))
 names(finalData) <- gsub("^t", "time", names(finalData))
 names(finalData) <- gsub("^f", "frequence", names(finalData))
 names(finalData) <- gsub("-mean", "Mean", names(finalData))
 names(finalData) <- gsub("-std", "Std", names(finalData))
 
-
+#create tidy data MeanData
 library(dplyr)
 groupData <- finalData %>%
         group_by(subject, activity) %>%
